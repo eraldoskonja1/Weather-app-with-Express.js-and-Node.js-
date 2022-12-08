2f7b1b2f8d6b4e7194f86982afb67c761ed63fc4
Eraldo <eraldoskonja1@gmail.com>
2022-12-08 18:33:18 +0100

    My weather project with Express.js and Node.js

---
+++ app.js
@@ -0,0 +1,49 @@
+// const { response } = require("express");
+const express = require ("express");
+const https = require ("https");
+const bodyParser = require ("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
+app.get("/", function(req, res){
+        
+    res.sendFile(__dirname + "/index.html")
+})
+
+app.post("/", function(req, res){
+   
+    const query = req.body.cityName
+    const apiKey = "185dbcc57e27f9315a49d3f1c762ebd7"
+    const units = "metric"
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&units=" + units +"&appid=" + apiKey
+
+    https.get(url, function(response){
+        console.log(response.statusCode);
+    
+    
+    response.on("data", function(data){
+        const weatherData = JSON.parse(data)
+        const temp = weatherData.main.temp
+        const weatherDescription = weatherData.weather[0].description
+        const icon = weatherData.weather[0].icon
+        const imgURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+        res.write("<p>The weather is currently " + weatherDescription + "</p>")
+        res.write("<h1>The temperature in " + query +" is " + temp + " degrees Celcius</h1>")  
+        res.write("<img src=" + imgURL + ">")    
+        res.send()      
+    
+})
+    
+})
+})
+    
+
+
+
+
+app.listen(3000, function(){
+    console.log("Server is running in port 3000.");
+})
